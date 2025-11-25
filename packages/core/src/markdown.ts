4e0aaa890d997465e9cd36dff6b0bd7243e40627
Mark Zebley <markzebley@gmail.com>
2025-11-25 09:26:29 -0600

    version bumping

--- packages/core/src/markdown.ts
+++ packages/core/src/markdown.ts
@@ -1,5 +1,11 @@
 import { marked } from "marked";
 
 export function renderMarkdown(markdown: string): string {
-  return marked.parse(markdown);
+  const html = marked.parse(markdown);
+
+  if (typeof html === "string") {
+    return html;
+  }
+
+  throw new Error("renderMarkdown unexpectedly returned a Promise");
 }
